5ba9c5a4dab4e26bfbe4c035ce9af6c247e7fa98
Godsmiracle001 <miraxzeegame@gmail.com>
2025-10-12 02:35:28 +0100

    feat:project page fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: "class", // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class",
+  safelist: [
+    "from-blue-500",
+    "to-purple-600",
+    "from-green-500",
+    "to-teal-600",
+    "from-purple-500",
+    "to-purple-700",
+    "from-pink-500",
+    "to-rose-600",
+    "from-orange-500",
+    "to-red-600",
+    "bg-blue-500",
+    "bg-green-500",
+    "bg-purple-500",
+    "bg-pink-500",
+    "bg-orange-500",
+  ],
   theme: {
     extend: {
       transitionProperty: {
